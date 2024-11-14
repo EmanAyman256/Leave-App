69270484e1ad16ce64a9f20581cf0c8ce7ba7ba0
EmanAyman256 <emanayman256@gmail.com>
2024-11-14 14:49:11 +0200

    Form-Update

--- src/app/activity-request-form/activity-request-form.component.ts
+++ src/app/activity-request-form/activity-request-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import {faUserPlus} from '@fortawesome/free-solid-svg-icons'
 import { map, Observable, startWith } from 'rxjs';
 interface User {
@@ -15,33 +15,45 @@ interface User {
 export class ActivityRequestFormComponent{
  plusUser=faUserPlus
  myControl = new FormControl<string|User>('');
-
- searchControl = new FormControl();
-
-
+searchControl = new FormControl();
 selectedItem: any;
 users: User[] = [
   { name: 'User 1', email: 'u1@example.com' },
   { name: 'User 2', email: 'u2@example.com' },
   { name: 'User 3', email: 'u3@example.com' },
-
-  
 ];
 options: string[] = ['One', 'Two', 'Three'];
 delUsers:string[] =['u1','u2','u3']
 approver:string[]=["PM1","TL2","PM2"]
 selectedDate:Date|null=null;
 filteredUsers!: Observable<User[]>;
-
+filteredAppr!:Observable<string>
+appForm=new FormGroup({
+  fullName:new FormControl('',[Validators.required]),
+  reqBy:new FormControl('',[Validators.required]),
+  activityType:new FormControl('',[Validators.required]),
+  duration:new FormControl('',[Validators.required]),
+  startDate:new FormControl(Date,[Validators.required]),
+  endDate:new FormControl(Date,[Validators.required]),
+  module:new FormControl('',[Validators.required]),
+  approvers:new FormControl('',Validators.required),
+  taskDele:new FormControl('',[Validators.required]),
+  attachSickLeave:new FormControl(null,[Validators.required]),
+  notes:new FormControl('')
+})
 ngOnInit() {
   this.filteredUsers = this.myControl.valueChanges.pipe(
     startWith(''),
     map(value => (typeof value === 'string' ? value : value?.name)),
-    map(name => (name ? this._filter(name) : this.users.slice()))
-  );
+    map(name => (name ? this._filter(name) : this.users.slice())));
+}
+onSubmit()
+{
+console.log(this.appForm);
+
 }
 
-private _filter(name: string): User[] {
+private _filter(name: string){
   const filterValue = name.toLowerCase();
   return this.users.filter(user => user.name.toLowerCase().includes(filterValue));
 }
